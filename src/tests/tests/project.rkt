55cf0689f43476acde14a8c37ada65a0bbd4e364
b2coutts <b2coutts@uwaterloo.ca>
2015-12-17 19:36:21 -0500

    Added test for RunC dependency checking

--- src/tests/tests/project.rkt
+++ src/tests/tests/project.rkt
@@ -5,6 +5,17 @@
          seashell/backend/runner
          seashell/seashell-config)
 
+(define test-hdr-file "int add(int, int);\n")
+(define test-imp-file "#include \"add.h\"\nint add(int a, int b){ return a+b; }\n")
+(define test-main-file #<<HERE
+#include <stdio.h>
+#include "add.h"
+int main(){
+    printf("%d\n", add(3,4));
+}
+HERE
+)
+
 (define/provide-test-suite project-suite
   (test-suite "Project Tests"
     (test-case "Create a Project"
@@ -52,6 +63,18 @@
       (define-values (success hsh) (compile-and-run-project "foo" "test.c" '()))
       (check-true success)
       (sync (program-wait-evt (hash-ref hsh 'pid))))
+
+    (test-case "Run a Project with tests"
+      (for ([file '("add.h" "add.c" "main.c" "tests/a.in" "test/a.expect")])
+           ([contents (list test-hdr-file test-imp-file test-main-file "3\n4\n" "7\n")])
+        (with-output-to-file (check-and-build-path (build-project-path "foo") file)
+          (thunk (display contents))))
+      (define-values (success hsh) (compile-and-run-project "foo" "main.c" (list "a")))
+      (check-true success)
+      (sync (program-wait-evt (hash-ref hsh 'pid)))
+      (check-true (match (sync (wrap-evt (program-stdout (hash-ref hsh 'pid)) (compose deserialize read)))
+          [(list _ _ "passed" _ _) #t]
+          [_ #f])))
 
     (test-case "Get a Compilation Error"
       (with-output-to-file (check-and-build-path (build-project-path "foo") "error.c")
